1d338977bda2a05e029ecd0735faff384d5f6fb7
SENTHIL-7 <apsenthilkumar2001@gmail.com>
2022-08-04 10:51:00 +0530

    class component state using tooglebutton

--- src/ToogleButton.js
+++ src/ToogleButton.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
 import './ToogleStyle.css';
 export default class ToogleButton extends Component {
+  constructor (props){
+    super(props);
+    this.state ={isOn:false}; 
+    this.changeToogle=this.changeToogle.bind(this);
+
+  }
+  changeToogle(){
+    this.setState(state=>({
+     isOn:!state.isOn
+    }))
+  }
   render() {
     return (
-      <button>change</button>
+      <button onClick={this.changeToogle}>
+        Toogle : {this.state.isOn?"on":"off"}</button>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import ShowTime from './ShowTime';
+
 import reportWebVitals from './reportWebVitals';
 import ToogleButton from './ToogleButton';
 
-function showTime(){
+
  
 
 
@@ -16,8 +17,8 @@ root.render(
  
 );
 
-}
-setInterval(showTime)
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
