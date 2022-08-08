f82370157d1f358f00dd32bec58fc5740fcafea7
SENTHIL-7 <apsenthilkumar2001@gmail.com>
2022-08-08 21:24:45 +0530

    hook useState

--- src/counder.js
+++ src/counder.js
@@ -1,17 +1,29 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 
-export default class Counder extends Component {
+// export default class Counder extends Component {
 
-    constructor(props){
-        super(props);
-        this.state={count:0}
-    }
-  render() {
-    return (
-      <div>
-        <p>the cound is : {this.state.count}</p>
-      <button onClick={()=>this.setState({count:this.state.count+1})}>Click</button>
-      </div>
-    )
-  }
+//     constructor(props){
+//         super(props);
+//         this.state={count:0}
+//     }
+//   render() {
+//     return (
+//       <div>
+//         <p>the cound is : {this.state.count}</p>
+//       <button onClick={()=>this.setState({count:this.state.count+1})}>Click</button>
+//       </div>
+//     )
+//   }
+// }
+import React, { useState } from 'react'
+
+export default function Counder() {
+
+    const[count,setCound]=useState(0);
+  return (
+    <div>
+         <p>the cound is : {count}</p>
+         <button onClick={()=>setCound(count+1)}>Click</button>
+    </div>
+  )
 }
