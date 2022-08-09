b7fc9abf041ff425f15d45eefe1453f25f8e87a9
SENTHIL-7 <apsenthilkumar2001@gmail.com>
2022-08-09 12:32:26 +0530

    hook useEfffect

--- src/counder.js
+++ src/counder.js
@@ -1,23 +1,12 @@
-// import React, { Component } from 'react'
-
-// export default class Counder extends Component {
-
-//     constructor(props){
-//         super(props);
-//         this.state={count:0}
-//     }
-//   render() {
-//     return (
-//       <div>
-//         <p>the cound is : {this.state.count}</p>
-//       <button onClick={()=>this.setState({count:this.state.count+1})}>Click</button>
-//       </div>
-//     )
-//   }
-// }
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Counder() {
+   
+  useEffect(
+    ()=>{
+      document.title=`count ${count}`
+    }
+  )
 
     const[count,setCound]=useState(0);
   return (
