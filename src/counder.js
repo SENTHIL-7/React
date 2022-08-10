ad3ce0c76b5da1e0eb12f099b021ca772abd8d1c
SENTHIL-7 <apsenthilkumar2001@gmail.com>
2022-08-10 20:48:08 +0530

    hoot rule don't do

--- src/counder.js
+++ src/counder.js
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from 'react'
 
 export default function Counder() {
    
-  useEffect(()=>{
-    setTimeout(()=>{setCount(count+1)},1000)
+  if(count<5){          //don't do this is not execute useeffect
+    useEffect(()=>{
+      setTimeout(()=>{setCount(count+1)},1000)
+    }
+    )
   }
-  )
 
     const[count,setCount] =useState(0);
  
